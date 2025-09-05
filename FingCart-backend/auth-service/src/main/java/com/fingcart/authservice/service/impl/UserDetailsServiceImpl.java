1210822519ca5382f1690e995688d4c9dcec6d4f
Theres Paul <therespaul77@gmail.com>
2025-09-05 14:27:56 +0530

    Implemented UserDetailsService

---
+++ FingCart-backend/auth-service/src/main/java/com/fingcart/authservice/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.fingcart.authservice.service.impl;
+
+import com.fingcart.authservice.entity.AppUser;
+
+import com.fingcart.authservice.repository.UserRepository;
+import lombok.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        AppUser user = userRepository.findByUsername(username).orElseThrow(
+                () ->  new UsernameNotFoundException("User with username "+username+ " not found")
+        );
+
+        return new User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
