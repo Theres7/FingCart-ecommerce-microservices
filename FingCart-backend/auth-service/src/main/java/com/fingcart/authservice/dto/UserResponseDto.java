b332c534266de4379e8ba64b207c4bb4b72bbc3f
Theres Paul <therespaul77@gmail.com>
2025-09-05 11:43:41 +0530

    Add User DTOs

---
+++ FingCart-backend/auth-service/src/main/java/com/fingcart/authservice/dto/UserResponseDto.java
@@ -0,0 +1,17 @@
+package com.fingcart.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+}
