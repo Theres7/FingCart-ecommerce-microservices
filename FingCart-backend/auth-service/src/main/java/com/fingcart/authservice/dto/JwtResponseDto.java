48c88865f081df84bba0fdd74d38c2cf629845ee
Theres Paul <therespaul77@gmail.com>
2025-09-05 14:44:10 +0530

    JWT response DTO

---
+++ FingCart-backend/auth-service/src/main/java/com/fingcart/authservice/dto/JwtResponseDto.java
@@ -0,0 +1,14 @@
+package com.fingcart.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponseDto {
+    private String token;
+}
