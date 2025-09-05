202c890cee18b76bdca75a667caa7d6f09a7aa53
Theres Paul <therespaul77@gmail.com>
2025-09-05 13:24:47 +0530

    Adds Jwt.java

---
+++ FingCart-backend/auth-service/src/main/java/com/fingcart/authservice/jwt/Jwt.java
@@ -0,0 +1,35 @@
+package com.fingcart.authservice.jwt;
+
+import com.fingcart.authservice.entity.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@RequiredArgsConstructor
+public class Jwt {
+    private final Claims claims;
+    private final SecretKey secretKey;
+
+    public boolean isExpired() {
+        return claims.getExpiration().before(new Date());
+    }
+
+    public boolean isValid() {
+        return claims.getExpiration().after(new Date());
+    }
+
+    public Long getUserId() {
+        return Long.valueOf(claims.getSubject());
+    }
+
+    public Role getRole() {
+        return Role.valueOf(claims.get("role", String.class));
+    }
+
+    public String toString() {
+        return Jwts.builder().claims(claims).signWith(secretKey).compact();
+    }
+}
