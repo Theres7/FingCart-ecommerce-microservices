5b737ea2a8bc4dce7c54fa51f5fd89dfc2e05fd5
Theres Paul <therespaul77@gmail.com>
2026-02-23 22:45:48 +0530

    Update cart-service

--- FingCart-backend/cart-service/src/main/java/com/fingcart/cartservice/exception/GlobalExceptionHandler.java
+++ FingCart-backend/cart-service/src/main/java/com/fingcart/cartservice/exception/GlobalExceptionHandler.java
@@ -3,13 +3,13 @@ package com.fingcart.cartservice.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
-@RestControllerAdvice
+@ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(CartNotFoundException.class)
